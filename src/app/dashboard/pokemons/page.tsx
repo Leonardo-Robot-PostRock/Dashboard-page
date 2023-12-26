86d719552b9c64175fafb910ba76019f2837f666
Leonardo <leonardo38361@gmail.com>
2023-12-26 19:45:33 -0300

    fech desde next

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,8 +1,17 @@
+const getPokemons = async ( limit = 20, offset = 0 ) => {
+  const data = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+    .then(res => res.json());
+    console.log(data);
+    
+  return data;
+}
 
-export default function PokemonsPage() {
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons();
+
   return (
     <div>
-      <h1>Hello Page</h1>
+      { JSON.stringify(pokemons) }
     </div>
   );
 }
