a35e5c31ae9e059761fc551d95e7b01c3b246d50
Leonardo <leonardo38361@gmail.com>
2024-01-18 00:24:25 -0300

    se mueven modulos afuera del sistema de rutas

--- src/app/pokemons/index.ts
+++
@@ -1,3 +0,0 @@
-export { PokemonsResponse } from './interface/pokemons-response';
-export { SimplePokemon } from './interface/simple-pokemon';
-export { PokemonGrid } from './components/PokemonGrid';
