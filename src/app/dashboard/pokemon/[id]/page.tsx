3c4e3fd4832b8183615a9ea31f4ee46d6424e462
Leonardo <leonardo38361@gmail.com>
2024-01-19 03:33:49 -0300

    Se carga información de pokemon por id

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,11 +1,25 @@
+import { Pokemon } from '@/pokemons';
+
 interface Props {
   params: { id: string };
 }
 
-export default function PokemonPage({ params }: Props) {
+const getPokemon = async (id: string): Promise<Pokemon> => {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    cache: 'force-cache',
+  }).then((resp) => resp.json());
+  console.log('Se cargó ', pokemon.name);
+
+  return pokemon;
+};
+
+export default async function PokemonPage({ params }: Props) {
+  const pokemon = await getPokemon(params.id);
+
   return (
     <div>
       <h1>Pokémon: {params.id}</h1>
+      <div>{JSON.stringify(pokemon)}</div>
     </div>
   );
 }
