11d0cfc3fae780fef93d954b5e8f16afdb0d0876
Leonardo <leonardo38361@gmail.com>
2024-01-30 19:33:47 -0300

    creación de componente favoritos

---
+++ src/app/dashboard/favorites/page.tsx
@@ -0,0 +1,19 @@
+import { PokemonGrid } from '@/pokemons';
+
+export const metada = {
+  title: 'Favoritos',
+  description: 'Se muestran a continuación 151 pokemons',
+};
+
+export default async function PokemonsPage() {
+  return (
+    <div className='p-2 flex flex-col'>
+      <span className='text-5xl my-2'>
+        Pokémons favoritos <small>Global state</small>
+      </span>
+      <div className='flex flex-wrap gap-10 items-center justify-center'>
+        <PokemonGrid pokemons={{}} />
+      </div>
+    </div>
+  );
+}
