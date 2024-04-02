e6b5d1dc82d756dba98f9126d2d5a836e51796c8
Leonardo <leonardo38361@gmail.com>
2024-04-02 11:38:10 -0300

    reducers

--- src/store/counter/counterSilce.ts
+++ src/store/counter/counterSilce.ts
@@ -1,19 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface CounterState {
   count: number;
+  isReady: boolean
 }
 
 const initialState: CounterState = {
   count: 5,
+  isReady: false
 };
 
 const counterSilce = createSlice({
   name: 'counter',
   initialState,
-  reducers: {},
+  reducers: {
+    addOne(state) {
+      state.count++;
+    },
+
+    substractOne(state) {
+      if (state.count === 0) return;
+      state.count--;
+    },
+
+    resetCounter(state, action: PayloadAction<number>) {
+      if (action.payload < 0) action.payload = 0;
+      state.count = action.payload;
+    }
+  },
 });
 
-export const {} = counterSilce.actions;
+export const { addOne, substractOne, resetCounter } = counterSilce.actions;
 
 export default counterSilce.reducer;
