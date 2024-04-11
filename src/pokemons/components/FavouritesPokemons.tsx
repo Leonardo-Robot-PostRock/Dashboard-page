7808b0eec79136e427798d73b9adce3fa28af2b5
Leonardo <leonardo38361@gmail.com>
2024-04-11 15:43:36 -0300

    mostrar caso de no favoritos

--- src/pokemons/components/FavouritesPokemons.tsx
+++ src/pokemons/components/FavouritesPokemons.tsx
@@ -1,11 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client"
 import { useAppSelector } from "@/store"
 import { PokemonGrid } from "./PokemonGrid";
+import { IoHeartOutline } from "react-icons/io5";
+import { useState } from "react";
 
 export const FavouritesPokemons = () => {
     const favouritesPokemons = useAppSelector(state => Object.values(state.pokemons));
+    const [pokemons, setPokemons] = useState(favouritesPokemons)
 
     return (
-        <PokemonGrid pokemons={favouritesPokemons} />
+        <>
+            {
+                !pokemons.length
+                    ? <NoFavourites />
+                    : <PokemonGrid pokemons={pokemons} />
+            }
+        </>
+    )
+}
+
+export default function NoFavourites() {
+    return (
+        <div className='flex flex-col h-[50vh] items-center justify-center'>
+            <IoHeartOutline size={100} className='text-red-500' />
+            <span>No hay favoritos</span>
+        </div>
     )
 }
