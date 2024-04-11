a786cde0b31aa1c432d2a0e8337a3a1bfc7ad1bd
Leonardo <leonardo38361@gmail.com>
2024-04-11 15:28:33 -0300

    nuevo componente en archivo de barril

--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -1,4 +1,5 @@
 export { PokemonGrid } from './components/PokemonGrid';
+export { FavouritesPokemons } from './components/FavouritesPokemons';
 
 export type { PokemonsResponse } from './interfaces/pokemons-response';
 export type { SimplePokemon } from './interfaces/simple-pokemon';
