aec2be6383e114da0637dad35e4c2c3e16e6af22
Leonardo <leonardo38361@gmail.com>
2024-04-18 01:01:25 -0300

    middleware comentado

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,16 @@
-import { configureStore, Middleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './counter/counter';
 import pokemonReducer from './pokemon/pokemon';
 
 import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
-import { localstorageMiddleware } from './middlewares/localstorage-middleware';
+// import { localstorageMiddleware } from './middlewares/localstorage-middleware';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     pokemons: pokemonReducer
   },
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(localstorageMiddleware as Middleware)
+  // middleware: getDefaultMiddleware => getDefaultMiddleware().concat(localstorageMiddleware as Middleware)
 
 });
 
