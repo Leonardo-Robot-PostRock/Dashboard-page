9e5e3fd7d01cc495ddeb653d083427bb07908222
Leonardo <leonardo38361@gmail.com>
2024-04-18 01:01:00 -0300

    creacion de nuevo reducer

--- src/store/pokemon/pokemon.ts
+++ src/store/pokemon/pokemon.ts
@@ -2,15 +2,17 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { SimplePokemon } from "@/pokemons";
 
 // const getInitialState = (): PokemonState => {
+//     if (typeof localStorage === 'undefined') return {};
 //     const favourites = JSON.parse(localStorage.getItem('favourite-pokemons') ?? '{}')
 //     return favourites;
 // }
 
 interface PokemonState {
-    [key: string]: SimplePokemon;
+    favourites: { [key: string]: SimplePokemon };
 }
 
 const initialState: PokemonState = {
+    favourites: {}
     // ...getInitialState(),
 }
 
@@ -18,16 +20,23 @@ const pokemonSlice = createSlice({
     name: 'pokemons',
     initialState,
     reducers: {
+
+        //Este state evita errores al cargar un estado que puede entrar en conflicto con lo que
+        //tiene Next del lado del servidor con el lado del cliente
+        setFavouritePokemons(state, action: PayloadAction<{ [key: string]: SimplePokemon }>) {
+            state.favourites = action.payload;
+        },
+
         toggleFavourite(state, action: PayloadAction<SimplePokemon>) {
             const pokemon = action.payload;
             const { id } = pokemon;
 
-            if (!!state[id]) {
-                delete state[id];
+            if (!!state.favourites[id]) {
+                delete state.favourites[id];
                 return;
             }
 
-            state[id] = pokemon;
+            state.favourites[id] = pokemon;
         }
     }
 });
